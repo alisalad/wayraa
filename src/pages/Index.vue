114411a1afc00cbb8ec9115bcb216428ba5ec67e
alisalad <Ali>
2020-06-14 22:50:08 +1000

    hero

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -34,7 +34,11 @@
       </div>
     </div>
     <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
+
+      <g-image src="~/img/blackman.svg" width="500"/>
+
+
+
     </div>
   </div>
 </section>
